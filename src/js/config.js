4e2f6a95add25eb23e6546d7217bc4c84994cf5f
Suryadhanasekar <suryad6201@gmail.com>
2023-02-07 14:48:34 +0530

    Recipe app initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = '321da8f1-787a-4c66-862b-393482563391';
+export const MODAL_CLOSE_SEC = 1;
---
+++ src/js/views/View.js
@@ -0,0 +1,95 @@
+import icons from '../../img/icons.svg';
+
+export default class View {
+  _data;
+
+  render(data, render = true) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+    this._data = data;
+    const markup = this._generateMarkup();
+
+    if (!render) {
+      return markup;
+    }
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  update(data) {
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const currentElements = Array.from(
+      this._parentElement.querySelectorAll('*')
+    );
+
+    //To avoid flicker while changing the servings.(instead of updating the dom we replaces the text)
+    newElements.forEach((newEl, i) => {
+      const currentEl = currentElements[i];
+
+      //Update changed text
+      if (
+        !newEl.isEqualNode(currentEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        currentEl.textContent = newEl.textContent;
+      }
+
+      //Update changed attribute
+      if (!newEl.isEqualNode(currentEl)) {
+        Array.from(newEl.attributes).forEach(attr =>
+          currentEl.setAttribute(attr.name, attr.value)
+        );
+      }
+    });
+  }
+
+  _clear() {
+    this._parentElement.innerHTML = '';
+  }
+
+  renderSpinner() {
+    const markup = `
+            <div class="spinner">
+              <svg>
+                <use href="${icons}#icon-loader"></use>
+              </svg>
+            </div>`;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  //If no error is passed then default shows
+  renderError(message = this._errorMessage) {
+    const markup = `
+      <div class="error">
+              <div>
+                <svg>
+                  <use href="${icons}#icon-alert-triangle"></use>
+                </svg>
+              </div>
+              <p>${message}</p>
+            </div>`;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  //Success message
+  renderMessage(message = this._message) {
+    const markup = `
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+}
